ee76b29d98d77fe236553bcfd26c5d9b6a47a901
Bogdana Pyvovarova <d.khalimonova@gmail.com>
2022-11-08 16:39:38 +0200

    2

--- js/task-02.js
+++ js/task-02.js
@@ -7,14 +7,39 @@ const ingredients = [
   "Condiments",
 ];
 
-const ul = document.querySelector("#ingredients");
-const li = ingredients.length;
-for (let index = 0; index < ingredients.length; index++) {
-  const ingredientsItem = document.createElement("li");
-  ingredientsItem.textContent = ingredients[index];
-  ingredientsItem.class = "item";
-  ul.append(ingredientsItem);
+const ingredientsEl = document.querySelector("#ingredients");
+const ingredientsItem = document.createElement("li");
+  ingredientsItem.textContent = ingredients;
+  ingredientsItem.classList.add("item");
+console.log(ingredientsItem);
+  
+ingredientsItem.li = ingredients;
+ingredientsEl.appendChild(ingredientsItem);
+console.log(ingredientsEl);
 
-  console.log(ul);
-  console.log(ingredientsItem.class);
-}
+
+
+
+
+
+
+
+
+// Напиши скрипт, який для кожного елемента масиву ingredients:
+
+// Створить окремий елемент <li>. Обов'язково використовуй метод document.createElement().
+// Додасть назву інгредієнта як його текстовий вміст.
+// Додасть елементу клас item.
+// Після чого, вставить усі <li> за одну операцію у список ul#ingredients.
+
+
+
+// for (let index = 0; index < ingredients.length; index++) {
+//   const ingredientsItem = document.createElement("li");
+//   ingredientsItem.textContent = ingredients[index];
+//   ingredientsItem.class = "item";
+//   ul.append(ingredientsItem);
+
+//   console.log(ul);
+//   console.log(ingredientsItem.class);
+// }
